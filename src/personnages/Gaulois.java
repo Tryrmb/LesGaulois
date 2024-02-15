6aae7d77e03508c7cfd1d9194ba38d674b39fe87
Tryrmb <tahiry.ramambzaafy@univ-tlse3.fr>
2024-02-15 17:43:05 +0100

    Troisième commit

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,17 +1,38 @@
 package personnages;
 
 public class Gaulois {
-	private Sring nom;
+	private String nom;
 	private int force;
 	private int effetPotion=1;
 	
 	
-	public Sring getNom() {
-		return nom;
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+
 	}
 
-
+	private String prendreParole() {
+		// TODO Auto-generated method stub
+		return "Le gaulois " + nom + " : ";
+	}
 	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de "+ romain.getNom());
+		romain.recevoirCoup(force/3);
+	}
 	
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion="+ effetPotion + "]";
+	}
+	
+	public static void main(String[] args) {
+		//TODO créer un main permettant de tester la classe Gaulois
+	}
 	
 }
