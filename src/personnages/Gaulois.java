d2a6316d2064115ec4d2c1d2f23af6a54a05082f
Tryrmb <tahiry.ramambzaafy@univ-tlse3.fr>
2024-02-15 11:56:00 +0100

    Deuxieme Commit

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,17 @@
 package personnages;
 
 public class Gaulois {
+	private Sring nom;
+	private int force;
+	private int effetPotion=1;
+	
+	
+	public Sring getNom() {
+		return nom;
+	}
 
+
+	
+	
+	
 }
